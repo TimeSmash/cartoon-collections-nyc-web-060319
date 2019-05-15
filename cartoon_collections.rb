be6db080dc960f4baa89dbc34c2b37f098577a06
mcumm64 <mcumm64@gmail.com>
2019-05-15 00:43:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(name_array)
-  name_array.each_with_index { |name, ind| puts "#{ind+1}. #{name}" }
+  name_array.each_with_index { |name, index| puts "#{index+1}. #{name}" }
 end
 
 def summon_captain_planet(calls)
