7af11ef16d2dfb2df752238d7ff286baaaee96e4
mcumm64 <mcumm64@gmail.com>
2019-05-15 00:41:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 def summon_captain_planet(calls)
   calls.collect do |call|
     # call = "#{call[0].upcase}#{call[1..call.length-1]}!"
-    "#{call.first.upcase.join(" ")}!"
+    call = "#{call[0].upcase.join("")}!"#{call[1..call.length-1]}!"
     # call << "!"
   end
 end
